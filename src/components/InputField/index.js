c8e2e334a9103efabc2475cae2491aa3cb37fb8d
David Yeung <quxbaz@gmail.com>
2020-10-07 02:27:31 -0600

    Coverted <InputField> to use css modules.

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,26 +1,8 @@
+import style from './index.css'
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {createApi} from '/api'
-
-/*
-  Styles
-*/
-
-const formStyle = {
-  display: 'flex',
-  marginBottom: '20px',
-  border: '1px solid #000',
-}
-
-const inputStyle = {
-  width: '100%',
-  height: '40px',
-  padding: '0 24px',
-  border: 'none',
-  outline: 'none',
-  fontSize: '14px',
-}
 
 function InputField ({onSubmit}) {
 
@@ -51,13 +33,13 @@ function InputField ({onSubmit}) {
   */
 
   return (
-    <form style={formStyle} onSubmit={handleSubmit}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <input
+        className={style.input}
         type='text'
         placeholder='What needs to be done?'
         autoFocus
         value={text}
-        style={inputStyle}
         onChange={handleChange} />
     </form>
   )
