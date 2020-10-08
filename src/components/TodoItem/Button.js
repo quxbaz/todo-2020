acf70245e30f2a22c0f113d0a9ad8bf016a1f6ec
David Yeung <quxbaz@gmail.com>
2020-10-07 20:27:56 -0600

    Cleaned up <Button>

--- src/components/TodoItem/Button.js
+++ src/components/TodoItem/Button.js
@@ -2,14 +2,10 @@ import css from './index.css'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({children, className, style, onClick}) => (
-  <div className={className} style={style} onClick={onClick}>
+const Button = ({children, ...props}) => (
+  <div {...props}>
     {children}
   </div>
 )
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
 
 export default Button
