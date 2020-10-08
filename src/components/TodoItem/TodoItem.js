85f40f5d3effe3c76f4dce3d2b345616a54911c2
David Yeung <quxbaz@gmail.com>
2020-10-07 19:43:51 -0600

    Support inline editing.

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,75 +1,43 @@
 import css from './index.css'
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {createApi} from '/api'
 import Checkbox from './Checkbox'
-import EditForm from './EditForm'
 
-function TodoItem ({todo, onToggle, onRemove, onEndEdit}) {
-
-  /*
-    State declarations
-  */
-
-  const [isEditing, setIsEditing] = useState(false)
-  const [editText, setEditText] = useState(todo.text)
-
-  /*
-    Event handlers
-  */
-
-  const handleEndEdit = (event) => {
-    event.preventDefault()
-    if (editText.trim() === '')
-      return
-    setIsEditing(false)
-    setEditText(editText.trim())
-    onEndEdit(todo.id, editText)
+const TodoItem = ({todo, onToggle, onRemove, onChange}) => {
+  const handleKeyDown = (event) => {
+    if (todo.text === '' && event.keyCode === 8)
+      onRemove(todo.id)
   }
-
-  /*
-    Rendering
-  */
-
-  const textStyle = {
-    paddingRight: '8px',
-    opacity: todo.isDone && 0.4,
-    textDecoration: todo.isDone && 'line-through',
-  }
-
   return (
     <div className={css.TodoItem}>
       <Checkbox
         checked={todo.isDone}
-        style={{visibility: isEditing ? 'hidden' : 'visible'}}
         onChange={() => onToggle(todo.id, !todo.isDone)} />
       <div style={{width: '100%'}}>
-        {isEditing ? (
-          <EditForm
-            value={editText}
-            onChange={(event) => setEditText(event.target.value)}
-            onSubmit={handleEndEdit} />
-        ) : (
-          <div style={textStyle} onClick={() => setIsEditing(true)}>
-            {todo.text}
-          </div>
-        )}
+        <input
+          value={todo.text}
+          className={css.Input}
+          style={{
+            opacity: todo.isDone ? 0.4 : 1,
+            textDecoration: todo.isDone ? 'line-through' : 'none',
+          }}
+          onChange={event => onChange(todo.id, event.target.value)}
+          onKeyDown={handleKeyDown} />
       </div>
-      {!isEditing && (
-        <button className={css.RemoveButton} onClick={() => onRemove(todo.id)}>
-          Remove
-        </button>)}
+      <button className={css.RemoveButton} onClick={() => onRemove(todo.id)}>
+        Remove
+      </button>
     </div>
   )
-
 }
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
   onToggle: PropTypes.func.isRequired,
   onRemove: PropTypes.func.isRequired,
-  onEndEdit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -81,7 +49,7 @@ const mapDispatchToProps = (dispatch) => {
     onRemove (id) {
       api.todos.remove(id)
     },
-    onEndEdit (id, text) {
+    onChange (id, text) {
       api.todos.update(id, {text})
     },
   }
