6740f73a2ae17deda2ed20ea0b7d2d35922f4743
David Yeung <quxbaz@gmail.com>
2020-10-12 15:13:29 -0600

    -Comment block

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,39 +1,4 @@
-/*
-
-  ::TODO::
-
-  The <Note> component should be the place describing its input
-  events. <List> should not be interpreting fine-grained events like
-  reading from event.keyCode. Instead <Note> should expose
-  onArrowUp/onArrowDown/onEnterKey functions.
-
-  For convenience it can package the exposure of these related event
-  handlers into something else instead of having all the logic being
-  wired in <Note>; something more generic and reusable.
-
-  Instead of plugging into every handler individually a la:
-
-    <Component
-      onArrowUp={...}
-      onArrowDown={...}
-      onEnterAtEndOfLine={...}
-      onEnterAtStartOfLine={...}
-      onEnterAtMiddleOfLine={...} />
-
-  You can perhaps package them up, or something.
-
-  Input should be a special input that handles the low-level event
-  logic and exposes high level callbacks.
-
-  Move caret should occur in the special <Input> component.
-
-  ::TODO::
-
-  - Is 'useRef' necessary?
-
-*/
-
-import React, {useRef} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {createApi} from '/api'
@@ -42,10 +7,8 @@ import handleNoteEvent from './handleNoteEvent'
 
 const List = ({list, notes, onEnterAtEnd}) => {
 
-  const ref = useRef()
-
   return (
-    <div ref={ref}>
+    <div>
       <h2>{list.title}</h2>
       <div>
         {notes.map((note, i) => (
