dcf730d7db375fd26b65c62588b07cfe175ca5a1
David Yeung <quxbaz@gmail.com>
2020-10-21 02:37:33 -0600

    Reduced code. Using css grid gap.

--- src/components/TrashView/TrashView.js
+++ src/components/TrashView/TrashView.js
@@ -4,7 +4,25 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {getSortedLists} from '/state/lists/selectors'
 
-const getColClassName = (i) => `${css.Entry} ${css.Col}${i % 3 + 1}`
+const Entry = ({list}) => (
+  <div className={css.Entry}>
+    <div className={css.EntryTitle}>
+      {list.title}
+    </div>
+    <div className={css.EntryInfo}>
+      {list.notes.length === 0
+        ? '- -'
+        : `${list.notes.length} notes`}
+      <a className={css.DeleteEntry}>
+        Delete
+      </a>
+    </div>
+  </div>
+)
+
+Entry.propTypes = {
+  list: PropTypes.object.isRequired,
+}
 
 const TrashView = ({lists, onEmptyTrash}) => {
   return (
@@ -14,19 +32,7 @@ const TrashView = ({lists, onEmptyTrash}) => {
       </header>
       <div className={css.TrashContent}>
         {lists.map((list, i) => (
-          <div key={list.id} className={getColClassName(i)}>
-            <div className={css.EntryContent}>
-              <div className={css.EntryTitle}>
-                {list.title} {i}
-              </div>
-              <div className={css.EntryInfo}>
-                {list.notes.length === 0
-                  ? '- -'
-                  : `${list.notes.length} notes`}
-                <a className={css.DeleteEntry}>DELETE</a>
-              </div>
-            </div>
-          </div>
+          <Entry key={list.id} list={list} />
         ))}
       </div>
     </div>
