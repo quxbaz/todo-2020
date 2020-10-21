19bd3f5847b94e6cedb295bda918d8de4ffab1ac
David Yeung <quxbaz@gmail.com>
2020-10-21 01:44:30 -0600

    Initial trash view.

--- src/components/TrashView/TrashView.js
+++ src/components/TrashView/TrashView.js
@@ -1,14 +1,44 @@
 import css from './style.css'
 import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {getSortedLists} from '/state/lists/selectors'
 
-const TrashView = () => {
+const getColClassName = (i) => `${css.Entry} ${css.Col}${i % 3 + 1}`
+
+const TrashView = ({lists, onEmptyTrash}) => {
   return (
     <div className={css.TrashView}>
-      TrashView
+      <header className={css.Header}>
+        <a onClick={onEmptyTrash}>Empty trash</a>
+      </header>
+      <div className={css.TrashContent}>
+        {lists.map((list, i) => (
+          <div key={list.id} className={getColClassName(i)}>
+            <div className={css.EntryContent}>
+              {list.title} {i}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-TrashView.propTypes = {}
+TrashView.propTypes = {
+  lists: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
 
-export default TrashView
+const mapStateToProps = (state) => ({
+  lists: getSortedLists(state, list => !list.isAlive),
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onEmptyTrash () {
+      console.log('EMPTY TRASH')
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrashView)
