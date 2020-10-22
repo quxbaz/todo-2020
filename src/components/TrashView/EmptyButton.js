f89adfa485ac79d979424aff97f5b554f5ffdb54
David Yeung <quxbaz@gmail.com>
2020-10-21 21:15:54 -0600

    Trash modal view.

---
+++ src/components/TrashView/EmptyButton.js
@@ -0,0 +1,56 @@
+import css from './style.css'
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {createApi} from '/api'
+import {createToast} from '/toasts'
+import {useEscapeKey} from '/hooks'
+import Modal from '/components/Modal'
+
+const EmptyButton = ({onEmptyTrash}) => {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const close = () => setIsOpen(false)
+  useEscapeKey(close)
+
+  return (
+    <div className={css.EmptyButton}>
+      {isOpen ? (
+        <Modal className={css.Modal} onClose={close}>
+          <div>Permanently delete all trash?</div>
+          <div className={css.ModalOptions}>
+            <button
+              onClick={close}
+              className={css.CloseButton}>Close</button>
+            <button
+              onClick={onEmptyTrash}
+              className={css.DeleteButton}>Empty all trash</button>
+          </div>
+        </Modal>
+      ) : (
+        <a onClick={() => setIsOpen(true)}>
+          Empty all trash
+        </a>
+      )}
+    </div>
+  )
+
+}
+
+EmptyButton.propTypes = {
+  onEmptyTrash: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = (dispatch) => {
+  const api = createApi(dispatch)
+  return {
+    onEmptyTrash () {
+      api.lists.emptyTrash()
+      createToast('toast-zone', {
+        text: 'Trashcan emptied.',
+      })
+    },
+  }
+}
+
+export default connect(null, mapDispatchToProps)(EmptyButton)
--- src/components/TrashView/TrashView.js
+++ src/components/TrashView/TrashView.js
@@ -6,6 +6,7 @@ import classNames from 'classnames'
 import {createApi} from '/api'
 import {getSortedLists} from '/state/lists/selectors'
 import EmptyView from './EmptyView'
+import EmptyButton from './EmptyButton'
 
 const Entry = ({list, onDeleteEntry}) => {
   const [isDeleting, setIsDeleting] = useState(false)
@@ -25,8 +26,8 @@ const Entry = ({list, onDeleteEntry}) => {
       </div>
       <div className={css.EntryInfo}>
         {list.notes.length === 0
-          ? '- -'
-          : `${list.notes.length} notes`}
+          ? <span style={{opacity: 0}}>-</span>
+          : `${list.notes.length} · notes`}
         <a
           className={css.DeleteEntry}
           title='Delete'
@@ -43,13 +44,13 @@ Entry.propTypes = {
   onDeleteEntry: PropTypes.func.isRequired,
 }
 
-const TrashView = ({lists, onEmptyTrash, onDeleteEntry}) => {
+const TrashView = ({lists, onDeleteEntry}) => {
   return lists.length === 0 ? (
     <EmptyView />
   ) : (
     <div className={css.TrashView}>
       <header className={css.Header}>
-        <a onClick={onEmptyTrash}>Empty trash</a>
+        <EmptyButton />
       </header>
       <div className={css.TrashContent}>
         {lists.map((list, i) => (
@@ -72,9 +73,6 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => {
   const api = createApi(dispatch)
   return {
-    onEmptyTrash () {
-      api.lists.emptyTrash()
-    },
     onDeleteEntry (id) {
       api.lists.destroy(id)
     },
