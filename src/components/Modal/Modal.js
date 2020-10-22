dbc738ea6917f4d40ec391f6b9a0d30c22371080
David Yeung <quxbaz@gmail.com>
2020-10-21 20:14:31 -0600

    components/Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,26 @@
+import css from './style.css'
+import React from 'react'
+import {createPortal} from 'react-dom'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import {useEscapeKey} from '/hooks'
+
+const Modal = ({children, className, onClose}) => {
+  useEscapeKey(onClose)
+  return createPortal(
+    <>
+      <div className={css.Backdrop} onClick={onClose} />
+      <div className={classNames(css.Modal, className)}>
+        {children}
+      </div>
+    </>,
+    document.getElementById('Modals')
+  )
+}
+
+Modal.propTypes = {
+  className: PropTypes.string,
+  onClose: PropTypes.func,
+}
+
+export default Modal
