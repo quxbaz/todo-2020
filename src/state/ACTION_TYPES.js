c4688a249d8444e889dcba19578db0c3283a711a
David Yeung <quxbaz@gmail.com>
2020-10-29 03:17:01 -0600

    Removed todo-2020/ namespace.

--- src/state/ACTION_TYPES.js
+++ src/state/ACTION_TYPES.js
@@ -1,31 +1,36 @@
 const ACTION_TYPES = {
 
   /*
+    history namespace
+  */
+  HISTORY__SET_URL: 'history/SET_URL',
+
+  /*
     workspace namespace
   */
-  WORKSPACE__SET_ACTIVE_LIST: 'todo-2020/workspace/SET_ACTIVE_LIST',
-  WORKSPACE__CYCLE_NEXT_LIST: 'todo-2020/workspace/CYCLE_NEXT_LIST',
-  WORKSPACE__CYCLE_PREV_LIST: 'todo-2020/workspace/CYCLE_PREV_LIST',
+  WORKSPACE__SET_ACTIVE_LIST: 'workspace/SET_ACTIVE_LIST',
+  WORKSPACE__CYCLE_NEXT_LIST: 'workspace/CYCLE_NEXT_LIST',
+  WORKSPACE__CYCLE_PREV_LIST: 'workspace/CYCLE_PREV_LIST',
 
   /*
     lists namespace
   */
-  LISTS__CREATE:          'todo-2020/lists/CREATE',
-  LISTS__UPDATE:          'todo-2020/lists/UPDATE',
-  LISTS__DISCARD:         'todo-2020/lists/DISCARD',
-  LISTS__DESTROY:         'todo-2020/lists/DESTROY',
-  LISTS__CREATE_NOTE:     'todo-2020/lists/CREATE_NOTE',
-  LISTS__DESTROY_NOTE:    'todo-2020/lists/DESTROY_NOTE',
-  LISTS__MERGE_NOTES:     'todo-2020/lists/MERGE_NOTES',
-  LISTS__CLEAR_NOTES:     'todo-2020/lists/CLEAR_NOTES',
-  LISTS__ORDER_NOTE_UP:   'todo-2020/lists/ORDER_NOTE_UP',
-  LISTS__ORDER_NOTE_DOWN: 'todo-2020/lists/ORDER_NOTE_DOWN',
-  LISTS__EMPTY_TRASH:     'todo-2020/lists/EMPTY_TRASH',
+  LISTS__CREATE:          'lists/CREATE',
+  LISTS__UPDATE:          'lists/UPDATE',
+  LISTS__DISCARD:         'lists/DISCARD',
+  LISTS__DESTROY:         'lists/DESTROY',
+  LISTS__CREATE_NOTE:     'lists/CREATE_NOTE',
+  LISTS__DESTROY_NOTE:    'lists/DESTROY_NOTE',
+  LISTS__MERGE_NOTES:     'lists/MERGE_NOTES',
+  LISTS__CLEAR_NOTES:     'lists/CLEAR_NOTES',
+  LISTS__ORDER_NOTE_UP:   'lists/ORDER_NOTE_UP',
+  LISTS__ORDER_NOTE_DOWN: 'lists/ORDER_NOTE_DOWN',
+  LISTS__EMPTY_TRASH:     'lists/EMPTY_TRASH',
 
   /*
     notes namespace
   */
-  NOTES__UPDATE: 'todo-2020/notes/UPDATE',
+  NOTES__UPDATE: 'notes/UPDATE',
 
 }
 
