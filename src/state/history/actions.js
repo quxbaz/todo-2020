986cce4591714d18e5e03c2d80356cdd2dd2b1b9
David Yeung <quxbaz@gmail.com>
2020-10-29 16:58:55 -0600

    Fixed action type.

--- src/state/history/actions.js
+++ src/state/history/actions.js
@@ -3,7 +3,7 @@ import ACTION_TYPES from 'state/ACTION_TYPES'
 const actions = {}
 
 actions.setUrl = (url) => ({
-  type: ACTION_TYPES.URL__SET_URL,
+  type: ACTION_TYPES.HISTORY__SET_URL,
   payload: {url},
 })
 
