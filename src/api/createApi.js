d473bfdb7012cf3b4358a9b58b5e3bdae6fae3b4
David Yeung <quxbaz@gmail.com>
2020-10-29 16:47:30 -0600

    API functions now provided entire api object.

--- src/api/createApi.js
+++ src/api/createApi.js
@@ -3,11 +3,15 @@ import workspace from './workspace'
 import lists from './lists'
 import notes from './notes'
 
-const createApi = (dispatch) => ({
-  history: history(dispatch),
-  workspace: workspace(dispatch),
-  lists: lists(dispatch),
-  notes: notes(dispatch),
-})
+const createApi = (dispatch) => {
+  const api = {}
+  Object.assign(api, {
+    history: history(dispatch, api),
+    workspace: workspace(dispatch, api),
+    lists: lists(dispatch, api),
+    notes: notes(dispatch, api),
+  })
+  return api
+}
 
 export default createApi
